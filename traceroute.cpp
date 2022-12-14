0f7950afbecc21bb166196e2b6554819d7cb1c99
Alexa Tello <tello.alexa@yahoo.com>
2022-12-14 08:34:49 -0600

    saving

--- traceroute.cpp
+++ traceroute.cpp
@@ -5,6 +5,8 @@
 #include <ctype.h>
 #include <stdlib.h>
 #include <netdb.h>
+#include<iostream>
+using namespace std;
 
 /* IP Header */
 struct ipheader {
@@ -28,6 +30,11 @@ struct icmpheader {
     unsigned short int icmp_id;     // Used in echo request/reply to identify request
     unsigned short int icmp_seq;    // Identifies the sequence of echo messages,
                                     // if more than one is sent.
+};
+
+struct headerpair {
+    struct ipheader iph;
+    struct icmpheader icmph;
 };
 
 #define ICMP_ECHO_REPLY     0
@@ -65,28 +72,9 @@ void traceroute(char* dest) {
     printf("traceroute to %s (%s), %d hops max, %ld bytes packets\n", dest, inet_ntoa(addr.sin_addr), MAX_HOPS, sizeof(ipheader) + sizeof(icmpheader));
     
     char send_buf[PACKET_LEN], recv_buf[PACKET_LEN];
-    /** TODO: 1
-     * Prepare packet
-     * a. outgoing packets only contain the icmpheader with type = ICMP_ECHO_REQUEST, code = 0
-     * b. ID in the icmpheader should be set to current process id to identify received ICMP packets
-     * c. checksum can be set to 0 for this test
-     * d. write/copy the header to the send_buf  
-     * 
-     * HINT:
-     * - icmpheader* icmp = (icmpheader*)send_buf;
-     * - set header fields with required values: icmp->field = value;
-     * */
-    
-    for (int ttl = 1; ttl <= MAX_HOPS; ) {
-        printf("%2d ", ttl);
-        /** TODO: 2
-         * set the seq in icmpheader to ttl
-         * 
-         * HINT:
-         * similar to TODO 1 HINT, just set the seq
-         */
-       
 
+    for(int ttl = 1; ttl <= MAX_HOPS; ) {
+        printf("%2d ", ttl);
         // set ttl to outgoing packets: no need to change
         if (setsockopt(sockfd, IPPROTO_IP, IP_TTL, (const char*)&ttl, sizeof(ttl)) < 0) {
             perror("setsockopt failed");
@@ -94,7 +82,34 @@ void traceroute(char* dest) {
         }
 
         int retry = 0;
-        while (1) {
+        while(true) {
+            // prepare ICMP packet
+            /** TODO: 1
+             * Prepare packet
+             * a. outgoing packets only contain the icmpheader with type = ICMP_ECHO_REQUEST, code = 0
+             * b. ID in the icmpheader should be set to current process id to identify received ICMP packets
+             * c. checksum can be set to 0 for this test
+             * d. write/copy the header to the send_buf  
+             * 
+             * HINT:
+             * - icmpheader* icmp = (icmpheader*)send_buf;
+             * - set header fields with required values: icmp->field = value;
+             * */
+            // prepare outgoing packets
+            icmpheader* icmp = (icmpheader*) send_buf;
+            icmp->icmp_type = ICMP_ECHO_REQUEST;
+            icmp->icmp_code = 0;
+            unsigned short int pid = (unsigned short int) getpid();
+            icmp->icmp_id = pid;
+            icmp->icmp_chksum = 0;
+            /** TODO: 2
+             * set the seq in icmpheader to ttl
+             * 
+             * HINT:
+             * similar to TODO 1 HINT, just set the seq
+             */
+            // update ttl of outgoing packet
+            icmp->icmp_seq = ttl;
             /** TODO: 3
              * send packet using sendto(...)
              * 
@@ -103,60 +118,92 @@ void traceroute(char* dest) {
              * - ensure we send one icmpheader in the packet
              * 
              */
-           
+            if(sendto(sockfd, send_buf, sizeof(icmpheader), 0, (sockaddr*)&addr, sizeof(addr)) != sizeof(icmpheader)) {
+                perror("packet didn't have exactly one icmpheader");
+                exit(-1);
+            }
+
+            socklen_t addr_len = sizeof(addr);
+            getpeername(sockfd, (struct sockaddr*) &addr, &addr_len);
+
+            // Print the destination address and port
+            char ip_str[INET_ADDRSTRLEN];
+            inet_ntop(AF_INET, &addr.sin_addr, ip_str, sizeof(ip_str));
+            printf("Destination address: %s\n", ip_str);
+            printf("Destination port: %d\n", ntohs(addr.sin_port));
+
+            timeval tv;
+            fd_set rfd;
            
             // wait to check if there is data available to receive; need to retry if timeout: no need to change
             tv.tv_sec = 1;
-            tv.tv_usec = 0;
             FD_ZERO(&rfd);
             FD_SET(sockfd, &rfd);
             int ret = select(sockfd + 1, &rfd, NULL, NULL, &tv);
+            if(ret == 0) {
+                // resend message
+                if(sendto(sockfd, (char*)icmp, sizeof(icmpheader), 0, (sockaddr*)&addr, sizeof(addr)) != sizeof(icmpheader)) {
+                    perror("packet didn't have exactly one icmpheader");
+                    exit(-1);
+                }
 
-            /** TOOD: 4
-             * Check if data available to read or timeout
-             * a. if ret == 0: timeout --> retry upto MAX_RETRY
-             * b. if ret > 0: data available, use recvfrom(...) to read data to recv_buf and process --> see TODO 5 below
-             */
-            if (ret == 0) {
-                // TODO 4.a
-            }
-            else if (ret > 0) {
-                // TODO 4.b
-                /** HINT: 
-                 * a. check man page of recvfrom, function returns bytes received
-                 * b. ensure data is received in recv_buf
-                 */
-                
-                /** TODO: 5
-                 * handle received packets based on received bytes
-                 * a. if (i) two pairs of ipheader and icmpheader received, (ii) type is TIME_EXCEEDED, (iii) sequence is the same as ttl, and (iv) id is same as pid
-                 *      --> print router ip and increment ttl to move on to processing next hop
-                 *      NOTE: first pair contains the ipheader and icmpheader created by the router; second pair would contain the original ipheader and icmpheader sent by us
-                 *  HINT:
-                 *    - check if bytes returned by recvfrom is at least 2 * (sizeof(ipheader) + sizeof(icmpheader))
-                 *    - if yes, use the icmpheader from the first pair to detect if the type is ICMP_TYPE_EXCEEDED
-                 *    - use the icmpheader from the second pair to match (i) seq == ttl, and (ii) id = pid
-                 * 
-                 * b. else if (i) type is ECHO_REPLY, and (ii) id is same as pid --> reached destination. Print ip and exit.
-                 * HINT:
-                 *    - should return only one icmpheader
-                 *    - match the type to ICMP_ECHO_REPLY and id to pid
-                 * c. otherwise ignore packet
-                 * 
-                 */
-               
-                // ----------------
-            }
-            else {
-                perror("select failed");
-                exit(-1);
+                // print attempt
+                cout << "* ";
+                retry++;
+            } else if(ret > 0) {
+                struct ipheader *ip = (struct ipheader*) recv_buf;
+                struct icmpheader *icmp = (struct icmpheader*) (recv_buf + sizeof(struct ipheader));
+                struct ipheader *ip2 = (struct ipheader*) (recv_buf + sizeof(struct ipheader) + sizeof(struct icmpheader));
+                struct icmpheader *icmp2 = (struct icmpheader*) (recv_buf + (2*sizeof(struct ipheader)) + sizeof(struct icmpheader));
+
+                socklen_t addr_len = sizeof(addr);
+                ssize_t bytes_read = recvfrom(sockfd, recv_buf, sizeof(recv_buf), 0, (sockaddr*)&addr, &addr_len);
+                if(bytes_read <= 0) {
+                    perror("no data read from recvfrom()");
+                    exit(-1);
+                }
+                struct ipheader ip_hdr;
+                memcpy(&ip_hdr, ip, sizeof(struct ipheader));
+                struct icmpheader icmp_hdr;
+                memcpy(&icmp_hdr, icmp, sizeof(struct icmpheader));
+
+                // get type and pid
+                unsigned char type = icmp_hdr.icmp_type;
+                char* router_ip;
+                router_ip = inet_ntoa(ip_hdr.iph_sourceip);
+
+                // evaluate bytes read by recvfrom()
+                if(bytes_read >= 2 * (sizeof(ipheader) + sizeof(icmpheader))) {  
+                    // create second set of headers
+                    struct ipheader ip_hdr2;
+                    memcpy(&ip_hdr2, ip2, sizeof(struct ipheader));
+                    struct icmpheader icmp_hdr2;
+                    memcpy(&icmp_hdr2, icmp2, sizeof(struct icmpheader)); 
+
+                    // check between header pairs 
+                    // && icmp_hdr.icmp_seq == icmp_hdr2.icmp_seq && icmp_hdr.icmp_id == icmp_hdr2.icmp_id
+                    if(type == ICMP_TIME_EXCEEDED) {
+                        // print router ip
+                        cout << router_ip;
+                        ttl++;
+                        break;
+                    } else {
+                        cout << "whomp";
+                    }
+                } else if(type == ICMP_ECHO_REPLY) {
+                    // print router ip
+                    cout << router_ip << endl;
+                    // exit
+                    return;
+                }
             }
             fflush(stdout);
-
-            /** TODO: 6
-             * Check if timed out for MAX_RETRY times; increment ttl to move on to processing next hop
-             */
+            if(retry == MAX_RETRY) {
+                ttl++;
+                break;
+            }
         }
+        cout << endl;
     }
     close(sockfd);
 }
